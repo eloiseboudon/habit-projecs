f2b84dffd6133730c4f94863d183caa36b31c456
eloiseboudon <boudon.eloise@gmail.com>
2025-10-04 20:17:30 +0200

    Add shim for STLLoader to fix expo-three resolution

---
+++ frontend/metro.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
+const MetroResolver = require('metro-resolver');
+
+const config = getDefaultConfig(__dirname);
+const previousResolveRequest = config.resolver.resolveRequest;
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'three/examples/js/loaders/STLLoader') {
+    return {
+      type: 'sourceFile',
+      filePath: path.resolve(__dirname, 'lib/shims/STLLoader.js'),
+    };
+  }
+
+  if (previousResolveRequest) {
+    return previousResolveRequest(context, moduleName, platform);
+  }
+
+  return MetroResolver.resolve(context, moduleName, platform);
+};
+
+module.exports = config;
